ee7f42ff4aae4bb0f02123aaaa298afbde28f417
S3len3Ch4vz <nasaya.369@gmail.com>
2021-12-08 21:01:40 -0600

    Inico sesión 3, lifecycle y components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import Greeting from "./components/Greeting"
-import Calculator from './components/Calculator';
+// Components
+import Counter from "./components/Counter";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React holi 
-        </a>
-        <Greeting name="Selene" lastName="Chávez"/>
-        <Calculator value1={1} value2={2} operator="suma"></Calculator>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+
+				<Counter />
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Counter/index.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+// Styles
+import "./Counter.css";
+
+export default function Counter() {
+	const [counter, setCounter] = useState(0);
+	const [otherCounter, setOtherCounter] = useState(0);
+
+	useEffect(() => {
+		console.warn("Me ejecuto  cada que cambie el estado other counter");
+	}, [otherCounter]);
+
+	useEffect(() => {
+		console.log("Me ejecuto solo la primera vez");
+	}, []);
+
+	useEffect(() => {
+		console.log("Me ejecuto cada que cambie el estado counter");
+		if (counter % 3 === 0 && counter > 0) {
+			setOtherCounter(otherCounter + 1);
+		}
+	}, [counter]);
+
+	return (
+		<div>
+			<p>Counter: {counter}</p>
+			<p>Counter 2: {otherCounter}</p>
+			<div className="container">
+				<button className="btn" onClick={() => setCounter(counter + 1)}>
+					Add
+				</button>
+				<button className="btn" onClick={() => setCounter(counter - 1)}>
+					Substract
+				</button>
+			</div>
+		</div>
+	);
+}
