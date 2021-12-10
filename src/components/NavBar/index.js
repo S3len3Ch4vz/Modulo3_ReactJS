3aaf0dab1c1963d89f60f689a596d10153557e4b
S3len3Ch4vz <nasaya.369@gmail.com>
2021-12-09 21:04:04 -0600

    Solucion ejercicio e inicio de sesion4

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,22 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
+// Styles
+import "./NavBar.css";
 
+function NavBar() {
+	const [active, setActive] = useState(3);
 
+	const handleClick = (itemID) => {
+		setActive(itemID);
+	};
 
+	const checkActive = (itemID) => (active === itemID ? "li-active" : "");
 
- function NavBar() {
-    
-   // let NavColor =0;
-    return (
-        <div>
-            <ul>
-            <li>Item1 </li>
-            <li>Item2 </li>
-            <li>Item3 </li>
-            </ul>
-            
-        </div>
-    )
+	return (
+		<div>
+			<ul>
+				<li className={checkActive(1)} onClick={(e) => handleClick(1)}>
+					Item 1
+				</li>
+				<li className={checkActive(2)} onClick={() => handleClick(2)}>
+					Item 2
+				</li>
+				<li className={checkActive(3)} onClick={() => handleClick(3)}>
+					Item 3
+				</li>
+			</ul>
+		</div>
+	);
 }
 
-export default NavBar
+export default NavBar;
