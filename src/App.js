581de84aa3cdb75530fde5752f67f52e7a35aebc
S3len3Ch4vz <nasaya.369@gmail.com>
2021-12-09 22:11:41 -0600

    Actualización ejercicio parte2 e inicio de bus de datos en espera

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 // Components
-import Counter from "./components/Counter";
-import NavBar from "./components/NavBar";
+//import Counter from "./components/Counter";
+//import NavBar from "./components/NavBar";
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 
-				<Counter />
-        <NavBar/>
+
 			</header>
 		</div>
 	);
