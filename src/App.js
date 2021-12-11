f5accd424a7a25569d978c06cd027c68936c340b
S3len3Ch4vz <nasaya.369@gmail.com>
2021-12-10 19:14:09 -0600

    Intento con el objeto json

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import "./App.css";
 
 // Components
 //import Counter from "./components/Counter";
-//import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar";
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-
+				
+				
+			<NavBar/>
 
 			</header>
+			
 		</div>
 	);
 }
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,22 +1,25 @@
 import React, {useState} from "react";
+import koders from "C:/Users/flor_/Downloads/KODEMIA/ReactJS/my-firts-app/src/helper/koders.json";
+
 
 // Styles
 import "./NavBar.css";
 
-function NavBar() {
+function NavBar(props) {
 	const [active, setActive] = useState(1);
-
+    console.log(koders)
 	const handleClick = (itemID) => {
 		setActive(itemID);
 	};
 
 	const checkActive = (itemID) => (active === itemID ? "li-active" : "");
 
+
 	return (
 		<div>
 			<ul>
 				<li className={checkActive(1)} onClick={(e) => handleClick(1)}>
-					Item 1
+					Item 1 
                     <div hidden ={checkActive(1) ? false : true}>Mucho Texto en 
                     la lista del Item 1</div>
 				</li>
