e23063428f0a9f66171a36df0e4c4953597e3fc7
S3len3Ch4vz <nasaya.369@gmail.com>
2021-12-21 21:16:09 -0600

    Evaluación de delete en proceso

--- src/pages/UsersList/index.js
+++ src/pages/UsersList/index.js
@@ -2,24 +2,19 @@ import React from "react";
 import { useState ,useEffect} from "react/cjs/react.development";
 
 //Services 
-import {getUsers, deleteUser, getUser} from "../../services/users";
+import {getUsers} from "../../services/users";
 
 //Components 
 import Li from "../../components/Li";
 
 //Hook
 import {useNavigate} from "react-router-dom";
-import { useParams } from "react-router-dom";
+
 
 function UsersList() {
-	const [firstName, setFirstName] = useState("");
-	const [lastName, setLastName] = useState("");
-	const [gender, setGender] = useState("");
-	const [occupation, setOccupation] = useState("");
-	const [birthday, setBirthday] = useState("");
+
 	const [users, setUsers] = useState([]);
     
-	const params = useParams();
     
 	const navigate = useNavigate();
 	useEffect(() => {
@@ -40,19 +35,7 @@ function UsersList() {
 
 	}, []);
 
-	useEffect(() => {
-		const getId = async () => {
-			const { firstName, lastName, gender, occupation, birthday } =
-				await getUser(params.userID);
-			// console.log(response);
-			setFirstName(firstName);
-			setLastName(lastName);
-			setGender(gender);
-			setOccupation(occupation);
-			setBirthday(birthday);
-		};
-		getId();
-	}, [params.userID]);
+
 
 	return (
 	<div>
